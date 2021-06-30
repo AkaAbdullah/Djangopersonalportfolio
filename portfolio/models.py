f953e3771fde6bf31c1b3dd867f5abe09882e9c3
Attari Ak <akaabdullah00@gmail.com>
2021-07-01 01:48:41 +0500

    second commit

--- portfolio/models.py
+++ portfolio/models.py
@@ -6,7 +6,8 @@ class Projects(models.Model):
     title = models.CharField(max_length=150)
     description = models.CharField(max_length=250)
     image = models.ImageField(upload_to="Uploads/")
-    url = models.URLField(blank=True)
+    videourl = models.URLField(blank=True)
+    giturl = models.URLField(blank=True)
 
     def __str__(self):
         return self.title
